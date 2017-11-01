6311467132cb0d36169eb1975c957758cbfb4c45
minakawa <minakawa.daiki.as@gmail.com>
2017-11-02 01:30:19 +0900

    [WIP]ニコニコのコメント取得

---
+++ app/script/renderer.js
@@ -0,0 +1,16 @@
+import Nico from "node-nicovideo-api";
+import Net from 'net';
+
+Nico.login("mail", "pass")
+  .then(session => session.live.getLiveInfo("lv308301105"))
+  .then(live =>{
+    const net = Net.connect(live._attr.comment.port, live._attr.comment.addr);
+    net.on('connect', () => {
+      net.setEncoding('utf-8');
+      net.write('<thread thread="'+live._attr.comment.thread+'" res_from="-5" version="20061206"/>\0');
+      console.log(net);
+    });
+    net.on('data',function(data){
+      console.log(data);
+    });
+  });
