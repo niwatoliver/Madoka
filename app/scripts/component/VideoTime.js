01535d9643d5a246f14e03d2abaa92db8b42be15
minakawa <minakawa.daiki.as@gmail.com>
2017-11-07 23:02:11 +0900

    経過時間の実装

--- app/scripts/component/VideoTime.js
+++ app/scripts/component/VideoTime.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 
+//TODO 過去に遡れないライブの場合の時間表示
 class VideoTime extends Component {
   constructor() {
     super();
-    this.youtubeUtil = window.youtubeUtil;
+    this.shuvi = null;
     this.state = { time: '00:00:00' };
+  }
+  componentDidMount() {
+    setInterval(() =>  {
+      if(!this.shuvi && window.shuvi){ this.shuvi = window.shuvi; }
+      if(this.shuvi && this.shuvi.current){
+        try {
+          this.setState({ time: VideoTime.computeDuration(this.shuvi.current()) });
+        } catch(e){ console.log('get time error'); }
+      }
+    }, 1000);
+  }
+
+  /**
+   * ミリ秒を時分秒へ変換
+   * ms ミリ秒
+   */
+  static computeDuration(ms){
+    const h = String(Math.floor(ms / 3600) + 100).substring(1);
+    const m = String(Math.floor((ms - h * 3600)/60)+ 100).substring(1);
+    const s = String(Math.round((ms - h * 3600 - m * 60))+ 100).substring(1);
+    return h+':'+m+':'+s;
   }
   render() {
     return (
