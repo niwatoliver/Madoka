dbe2efd7e3d3e86d2a7163d014bca037064ef444
minakawa <minakawa.daiki.as@gmail.com>
2017-11-12 19:22:24 +0900

    圧縮対応

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,6 @@ const webpack = require('webpack');
 const path = require('path');
 require('dotenv').config({ path: __dirname + '/.env' });
 
-const defineEnv = new webpack.DefinePlugin({
-  'process.env': { 'YOUTUBE_API_KRY': JSON.stringify(process.env.YOUTUBE_API_KRY) }
-});
-
 module.exports = {
   entry: './app/scripts/app.js',
   output: {
@@ -28,5 +24,20 @@ module.exports = {
     'fs',
     'googleapis'
   ],
-  plugins: [defineEnv]
+  /* プラグインの設定 */
+  plugins: [
+    /* DefinePluginの実行 */
+    new webpack.DefinePlugin({
+      // process.env.NODE_ENVを'production'に置き換える
+      'process.env.NODE_ENV': JSON.stringify('production'),
+      'process.env.YOUTUBE_API_KRY': JSON.stringify(process.env.YOUTUBE_API_KRY)
+    }),
+    /* UglifyJsPluginの実行 */
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        // 圧縮する時に警告を除去する
+        warnings: false
+      }
+    })
+  ]
 };
